0814a55258da1ef131ffbafc106e7ea5e72b6cbe
Shnip1906 <silva.pedro@ipvc.pt>
2023-06-08 15:38:47 +0100

    Main

--- ES2_TP/Infrastructure/docker/images/pg/migrations/009-create-propostas-table.sql
+++
@@ -1,8 +0,0 @@
-create table propostas(
-                          id_propostas uuid primary key default uuid_generate_v4(),
-                          nome_propostas varchar(100) not null,
-                          nTotalHoras int not null,
-                          descricao varchar(1000) not null,
-                          id_area_profissional uuid,
-                          FOREIGN KEY (id_area_profissional) REFERENCES areaProfissional (id_area_profissional)
-)
