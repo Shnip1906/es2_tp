d1eaa8d64ebabd0f786fee29921bbc8c8a082be0
Pedro Silva <77203606+Shnip1906@users.noreply.github.com>
2023-06-08 15:26:42 +0100

    Revert "Create ententies"

--- ES2_TP/Infrastructure/docker/images/pg/migrations/002-create-experiencia-table.sql
+++
@@ -1,8 +0,0 @@
-create table experiencia(
-    id_experiencia uuid primary key default uuid_generate_v4(),
-    nome_experiencia varchar(100) not null,
-    nome_empresa varchar(100) not null,
-    anoInicio int not null,
-    anoFim int not null,
-    continuo boolean default false
-);
--- ES2_TP/Infrastructure/docker/images/pg/migrations/010-create-skillsProposta-table.sql
+++
@@ -1,8 +0,0 @@
-create table skillsProposta(
-                               id_skills_proposta uuid primary key default uuid_generate_v4(),
-                               n_minimo_horas_skill int not null,
-                               id_skills uuid,
-                               id_propostas uuid,
-                               FOREIGN KEY (id_skills) REFERENCES skills (id_skills),
-                               FOREIGN KEY (id_propostas) REFERENCES propostas (id_propostas)
-)
