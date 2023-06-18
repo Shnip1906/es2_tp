b6e397551010fe9a1911daf24ea885ceb29fe28a
pdanielferreira <pedro.daniel.duarte@hotmail.com>
2023-06-18 23:26:40 +0100

    Bug Fix

--- ES2_TP/BusinessLogic/Context/MyDbContext.cs
+++ ES2_TP/BusinessLogic/Context/MyDbContext.cs
@@ -36,7 +36,7 @@ public partial class MyDbContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseNpgsql("Host=localhost;Database=es2;Username=es2;Password=es2;Port=52654");
+        => optionsBuilder.UseNpgsql("Host=localhost;Database=es2;Username=es2;Password=es2;Port=49684");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
